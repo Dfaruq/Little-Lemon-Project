b0f6a973d1a82f8ee12b0aa56eff40d72288b118
Dfaruq <daanyalfaruq@gmail.com>
2024-05-29 17:48:10 +0100

    CSS styling 60%

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,17 @@
 import './index.css';
+import heroImage from './hero-image.jpg';
 
 function Main() {
     return (
     <main>
       <div id="hero">
+       <div className='hero-text'>
         <h2>Little Lemon</h2>
         <h3>Chicago</h3>
         <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+        <button className="reserve-button">Reserve a Table</button>
+       </div>
+        <img src={heroImage} alt="A plate of appetisers" className='hero-image'/>
       </div>
       <div id="specials">
         <h3>Specials</h3>
